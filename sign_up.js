2bdda8016e5bc736c4ad14acee13866cb35dc21c
Ojas vats Tyagi <137185464+ojasvatstyagi@users.noreply.github.com>
2023-06-20 21:46:24 +0530

    Delete sign_up.js

--- sign_up.js
+++
@@ -1,4 +0,0 @@
-const firstName = document.getElementById("firstName").value
-const lastName = document.getElementById("lastName").value
-const fullName = firstName + " " + lastName
-export const name = () =>{return fullName}
