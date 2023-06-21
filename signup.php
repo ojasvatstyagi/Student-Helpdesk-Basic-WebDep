9bcb71f244bf1c4d010c7e70ec357b1d3107fd7c
aaravsharma17 <118906362+aaravsharma17@users.noreply.github.com>
2023-06-21 22:57:56 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,11 @@
+<?php$con= mysqli_connect('localhost','root','','user');
+if (!$con) {    die("Connection failed: " . mysqli_connect_error());}
+$firstName=$_POST["firstName"];
+$lastName=$_POST["lastName"];
+$userName=$_POST["userName"];
+$email=$_POST["email"];
+$password=$_POST["password"];
+$repeatPassword=$_POST["repeatPassword"];
+$sql="insert into account values('".$firstName."','".$lastName."','".$userName."','".$email."','".$password."','".$repeatPassword."');";
+$rs = mysqli_query($con,$sql);
+if($rs){ echo '<!DOCTYPE html><html>    <head>        <title>My Web page</title>        <link rel="stylesheet" href="6.css">     </head>     <body>        <div class="container">            <div class="content">                <h1>The Account is Successfully Created!!</h1>                <a href="uidpro.html"> Click here to go to login page </a>             </div>          </div>      </body> </html>            '; }?>
