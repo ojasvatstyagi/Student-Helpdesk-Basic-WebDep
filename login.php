181977f83fa90c07a25b082186b12b7ef654ea91
aaravsharma17 <118906362+aaravsharma17@users.noreply.github.com>
2023-06-24 00:45:08 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,25 +1,27 @@
 <?php
-if (!isset($_GET['submit'])) 
-{  
-$useName= $_GET["userName"];  
-$password = $_GET["password"];
-$repeatPassword= $_GET["repeatPassword"];  
-$con= mysqli_connect('localhost','root','','user');  
-$sql="select * from account where userName='".$userName."' and password='".$password."' and repeatPassword='".$repeatPassword."';";
+   
+$useName= $_POST["userName"];  
+$password = $_POST["password"];
+$repeatPassword= $_POST["repeatPassword"];  
+$con= mysqli_connect('localhost','root','','uid');  
+$sql="select * from tab1 where userName='".$userName."' and password='".$password."' and repeatPassword='".$repeatPassword."';";
   echo $sql;  
   $result=mysqli_query($con,$sql);  
   $resultcheck=mysqli_num_rows($result);  
   if($resultcheck>0)  
   { 
-  echo '<html> 
+  echo 
+  '<html> 
   <body> 
-  <script> location.replace("login_page.html"); 
+  <script> location.replace("index2.html"); 
   </script> 
   </body> 
   </html>'; 
-  shell_exec("touch cookie"); $com7="echo '".$email1."' >> cookie "; shell_exec($com7);  }  
+  }
+  
   else  
-  {  echo '<!DOCTYPE html>
+  {  
+  echo '<!DOCTYPE html>
   <html>    
   <head>        
   <title>My Web page</title>             
@@ -31,9 +33,11 @@ $sql="select * from account where userName='".$userName."' and password='".$pass
   </div>          
   </div>      
   </body> 
-  </html>            ';  
+  </html> ';  
+  
+  
   }  
-  }
-  mysqli_close($con);?>
-
+  
+  
+  
 
